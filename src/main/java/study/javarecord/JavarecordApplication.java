bc64701a9b074148407617c2fd3a9f4bc5518e77
juhyeon1114 <juhyeon1114@gmail.com>
2024-07-26 17:39:40 +0900

    init

---
+++ src/main/java/study/javarecord/JavarecordApplication.java
@@ -0,0 +1,13 @@
+package study.javarecord;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavarecordApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavarecordApplication.class, args);
+	}
+
+}
---
+++ src/test/java/study/javarecord/JavarecordApplicationTests.java
@@ -0,0 +1,13 @@
+package study.javarecord;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavarecordApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
